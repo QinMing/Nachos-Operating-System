02f80453bb5b24cee56fea1adaf95e7280de70f9
He Lonnie Liu <liulonnie@gmail.com>
2014-10-04 16:23:41 -0700

    shell

--- code/test/shell.c
+++ code/test/shell.c
@@ -1,33 +1,29 @@
 #include "syscall.h"
 
 int
-main()
-{
+main() {
     SpaceId newProc;
     OpenFileId input = ConsoleInput;
     OpenFileId output = ConsoleOutput;
     char prompt[2], ch, buffer[60];
     int i;
 
-    prompt[0] = '-';
-    prompt[1] = '-';
+    prompt[0] = '$';
+    prompt[1] = ' ';
 
-    while( 1 )
-    {
+    while (1) {
         Write(prompt, 2, output);
 
         i = 0;
 
         do {
-
             Read(&buffer[i], 1, input);
-
-        } while( buffer[i++] != '\n' );
+        } while (buffer[i++] != '\n');
 
         buffer[--i] = '\0';
 
-        if( i > 0 ) {
-            newProc = Exec(buffer);
+        if (i > 0) {
+            newProc = Exec(buffer, 0, 0, 1);
             Join(newProc);
         }
     }
--- code/userprog/syscall.h
+++ code/userprog/syscall.h
@@ -57,7 +57,7 @@ typedef int SpaceId;
 /* Run the executable, stored in the Nachos file "name", and return the
  * address space identifier
  */
-SpaceId Exec(char *name);
+SpaceId Exec(char *name, int argc, char **argv, int opt);
 
 /* Only return once the the user program "id" has finished.
  * Return the exit status.
