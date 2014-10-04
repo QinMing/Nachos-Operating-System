5604e1c39cfa8eec9f4736be5f571a25a536fcad
He Lonnie Liu <liulonnie@gmail.com>
2014-10-04 16:12:39 -0700

    fixing the sort program

--- code/test/sort.c
+++ code/test/sort.c
@@ -17,16 +17,20 @@ main()
     int i, j, tmp;
 
     /* first initialize the array, in reverse sorted order */
-    for (i = 0; i < 1024; i++)
-        A[i] = 1024 - i;
+    for (i = 0; i < 1024; i++) {
+        A[i] = 1023 - i;
+    }
 
     /* then sort! */
-    for (i = 0; i < 1023; i++)
-        for (j = i; j < (1023 - i); j++)
-            if (A[j] > A[j + 1]) {	/* out of order -> need to swap ! */
+    for (i = 0; i < 1023; i++) {
+        for (j = 0; j < i; j++) {
+            if (A[i] > A[j]) {	/* out of order -> need to swap ! */
                 tmp = A[j];
-                A[j] = A[j + 1];
-                A[j + 1] = tmp;
+                A[j] = A[i];
+                A[i] = tmp;
             }
+        }
+    }
+
     Exit(A[0]);		/* and then we're done -- should be 0! */
 }
