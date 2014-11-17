afa409e3fd2ef45c5b083570c57c6aac7d0aa48f
Ming Qin <mingqin@ucsd.edu>
2014-11-17 12:46:20 -0800

    create Table class

---
+++ code/userprog/table.cc
@@ -0,0 +1,55 @@
+#include "copyright.h"
+#include "table.h"
+
+/* Create a table to hold at most "size" entries. */
+Table::Table(int size){
+	tabsize = size;
+	tab = new void*[size];
+	inUse = new bool[size];
+	for (int i=0;i<size;i++){
+		tab[i] = NULL;
+		inUse[i] = false;
+	}
+	lock = new Lock("TableLock");
+}
+Table::~Table(){
+	delete[] tab;
+	delete lock;
+}
+
+/* Allocate a table slot for "object", returning the "index" of the
+allocated entry; otherwise, return -1 if no free slots are available. */
+int Table::Alloc(void *object){
+	lock->Acquire();
+	for (int i=0;i<tabsize;i++){
+		if ( inUse[i]==false){
+			inUse[i]=true;
+			lock->Release();
+			return i;
+		}
+	}
+	lock->Release();
+	return -1;
+}
+
+/* Retrieve the object from table slot at "index", or NULL if that
+slot has not been allocated. */
+void* Table::Get(int index){
+	lock->Acquire();
+	if (inUse[index]==false){
+		lock->Release();
+		return NULL;
+	}else{
+		lock->Release();
+		return tab[index];
+	}
+}
+
+/* Free the table slot at index. */
+//assuming the object being pointed has already been deleted.
+void Table::Release(int index){
+	lock->Acquire();
+	ASSERT(inUse[index]);//panic if the slot is not allocated
+	inUse[index]=false;
+	lock->Release();
+}
---
+++ code/userprog/table.h
@@ -0,0 +1,31 @@
+#ifndef TABLE_H
+#define TABLE_H
+
+#include "copyright.h"
+#include "synch.h"
+
+class Table {
+public:
+		/* Create a table to hold at most "size" entries. */
+	Table(int size);
+	~Table();
+
+		/* Allocate a table slot for "object", returning the "index" of the
+		allocated entry; otherwise, return -1 if no free slots are available. */
+	int Alloc(void *object);
+
+		/* Retrieve the object from table slot at "index", or NULL if that
+		slot has not been allocated. */
+	void *Get(int index) ;
+
+		/* Free the table slot at index. */
+	void Release(int index) ;
+
+private:
+	void** tab;
+	bool* inUse;
+	int tabsize;
+	Lock* lock;
+};
+
+#endif
