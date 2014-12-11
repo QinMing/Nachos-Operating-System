d3bef2ac568cd8121585e89f5716b4c58036f510
Ming Qin <mingqin@ucsd.edu>
2014-12-11 14:16:24 -0800

    comparing with old code

--- code/machine/translate.cc
+++ code/machine/translate.cc
@@ -211,7 +211,8 @@ Machine::Translate(int virtAddr, int* physAddr, int size, bool writing)
 
 // calculate the virtual page number, and offset within the page,
 // from the virtual address
-    vpn = (unsigned) virtAddr / PageSize;printf("\tTranslate %d, %s: ", vpn, writing ? "write" : "read");
+    vpn = (unsigned) virtAddr / PageSize;
+	if(currentThread->processId==1)printf("\tTranslate %d,%s", vpn, writing ? "write" : "read");
     offset = (unsigned) virtAddr % PageSize;
 
     if (tlb == NULL) {		// => page table => vpn is index into table
